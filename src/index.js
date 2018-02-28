e1fb77d3370bc9b090c7c80d3b917f9c87924788
Alexey Ostapenko <alexeyostapenko@Air-Alexey.Dlink>
2018-02-28 22:35:24 +0300

    advanced zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let zerosCount = 0;
+  let multiplier = base;
+
+  
+  if (multiplier === 2)
+  {
+    while (multiplier <= number)
+    {
+      zerosCount += Math.floor(number/multiplier);
+      multiplier *= 2; 
+    }
+      return zerosCount;
+  }
+
+  for (i = 2; i < base; i++)
+{
+  while (multiplier%i === 0 && multiplier !== i )
+  {
+   multiplier /=i; 
+  }
+}
+ const baseMultiplier = multiplier;
+ 
+ while (multiplier <= number)
+ {
+  zerosCount += Math.floor(number/multiplier);
+   multiplier *= baseMultiplier; 
+ }
+ 
+ return zerosCount;
 }
